01f50ca51dd5c1a217126bdb45ed5d165a4d02db
ronlu <ronlu057@gmail.com>
2023-10-09 12:14:41 +0800

    01

--- src/views/AboutUs.vue
+++ src/views/AboutUs.vue
@@ -47,13 +47,11 @@
     background: linear-gradient(to bottom, rgba(37, 168, 93, 1) 0%, rgba(14, 126, 134, 1) 100%);
     filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='#25a85d', endColorstr='#0e7e86', GradientType=0);
 }
-
 .about_box2_in {
     background-color: #FFF;
     margin-left: 15px;
     padding: 40px 0;
 }
-
 .text_RL2 {
     display: flex;
     justify-content: space-between;
@@ -63,17 +61,14 @@
     max-width: 80vw;
     flex-direction: row-reverse;
 }
-
 .txt_Lb {
     width: 40%;
 }
-
 .txt_Lb img {
     max-width: 100%;
     border: 0;
     vertical-align: middle;
 }
-
 .txt_Rb {
     width: 55%;
 }
@@ -88,19 +83,16 @@
     padding-bottom: 40px;
     float: left;
 }
-
 .txt_sty01 {
     color: #468f74;
     font-size: 20px;
     line-height: 20px;
     font-family: "Outfit";
 }
-
 .txt_sty02 {
     font-size: 1.4rem;
     line-height: 1.71;
 }
-
 .abot2_one {
     border-radius: 10px;
     border: 1px solid #CCCCCC;
@@ -117,7 +109,6 @@
     align-items: center;
     flex-wrap: wrap;
 }
-
 .item_txtbox3 h2 {
     width: 100%;
     font-size: 20px;
@@ -127,20 +118,17 @@
     padding-bottom: 5px;
     margin-bottom: 10px;
 }
-
 .item_txtbox3>ul {
     width: 100%;
     height: auto;
     float: left;
 }
-
 .item_txtbox3>ul>li {
     display: inline-flex;
     justify-content: flex-start;
     align-content: flex-start;
     align-items: stretch;
 }
-
 .item_txtbox3>ul>li>svg {
     font-size: 0.7rem;
     color: #468f74;
